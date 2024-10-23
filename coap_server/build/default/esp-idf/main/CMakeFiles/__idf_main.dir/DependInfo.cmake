5cd01081d770886bc32e16c4efb9bae58e16e017
CeicGitHub <cesar.inda@iteso.mx>
2024-10-22 19:02:14 -0600

    ProjectBase_coAP-Server

---
+++ coap_server/build/default/esp-idf/main/CMakeFiles/__idf_main.dir/DependInfo.cmake
@@ -0,0 +1,159 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/tiori/workspace/Practice3_752964/coap_server/build/default/coap_ca.pem.S" "C:/Users/tiori/workspace/Practice3_752964/coap_server/build/default/esp-idf/main/CMakeFiles/__idf_main.dir/__/__/coap_ca.pem.S.obj"
+  "C:/Users/tiori/workspace/Practice3_752964/coap_server/build/default/coap_oscore.conf.S" "C:/Users/tiori/workspace/Practice3_752964/coap_server/build/default/esp-idf/main/CMakeFiles/__idf_main.dir/__/__/coap_oscore.conf.S.obj"
+  "C:/Users/tiori/workspace/Practice3_752964/coap_server/build/default/coap_server.crt.S" "C:/Users/tiori/workspace/Practice3_752964/coap_server/build/default/esp-idf/main/CMakeFiles/__idf_main.dir/__/__/coap_server.crt.S.obj"
+  "C:/Users/tiori/workspace/Practice3_752964/coap_server/build/default/coap_server.key.S" "C:/Users/tiori/workspace/Practice3_752964/coap_server/build/default/esp-idf/main/CMakeFiles/__idf_main.dir/__/__/coap_server.key.S.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "ESP_PLATFORM"
+  "IDF_VER=\"HEAD-HASH-NOTFOUND\""
+  "MBEDTLS_CONFIG_FILE=\"mbedtls/esp_config.h\""
+  "SOC_MMU_PAGE_SIZE=CONFIG_MMU_PAGE_SIZE"
+  "SOC_XTAL_FREQ_MHZ=CONFIG_XTAL_FREQ"
+  "_GLIBCXX_HAVE_POSIX_SEMAPHORE"
+  "_GLIBCXX_USE_POSIX_SEMAPHORE"
+  "_GNU_SOURCE"
+  "_POSIX_READER_WRITER_LOCKS"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "config"
+  "C:/Users/tiori/workspace/Practice3_752964/coap_server/main"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/newlib/platform_include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/freertos/config/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/freertos/config/include/freertos"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/freertos/config/xtensa/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/freertos/FreeRTOS-Kernel/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/freertos/FreeRTOS-Kernel/portable/xtensa/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/freertos/FreeRTOS-Kernel/portable/xtensa/include/freertos"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/freertos/esp_additions/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_hw_support/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_hw_support/include/soc"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_hw_support/include/soc/esp32s3"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_hw_support/dma/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_hw_support/ldo/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_hw_support/port/esp32s3/."
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_hw_support/port/esp32s3/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/heap/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/log/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/soc/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/soc/esp32s3"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/soc/esp32s3/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/hal/platform_port/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/hal/esp32s3/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/hal/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_rom/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_rom/include/esp32s3"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_rom/esp32s3"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_common/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_system/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_system/port/soc"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_system/port/include/private"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/xtensa/esp32s3/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/xtensa/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/xtensa/deprecated_include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/lwip/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/lwip/include/apps"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/lwip/include/apps/sntp"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/lwip/lwip/src/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/lwip/port/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/lwip/port/freertos/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/lwip/port/esp32xx/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/lwip/port/esp32xx/include/arch"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/lwip/port/esp32xx/include/sys"
+  "C:/Users/tiori/workspace/Practice3_752964/coap_server/managed_components/espressif__coap/port/include"
+  "C:/Users/tiori/workspace/Practice3_752964/coap_server/managed_components/espressif__coap/libcoap/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/mbedtls/port/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/mbedtls/mbedtls/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/mbedtls/mbedtls/library"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/mbedtls/esp_crt_bundle/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/mbedtls/mbedtls/3rdparty/everest/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/mbedtls/mbedtls/3rdparty/p256-m"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/mbedtls/mbedtls/3rdparty/p256-m/p256-m"
+  "C:/Users/tiori/esp/v5.3.1/esp-idf/examples/common_components/protocol_examples_common/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_gpio/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_timer/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_pm/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_app_format/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_bootloader_format/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/app_update/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/bootloader_support/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/bootloader_support/bootloader_flash/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_partition/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/efuse/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/efuse/esp32s3/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_mm/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/spi_flash/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/pthread/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_event/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_ringbuf/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_spi/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_eth/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_uart/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/vfs/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_usb_serial_jtag/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_vfs_console/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_netif/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_pcnt/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_gptimer/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_mcpwm/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_ana_cmpr/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_i2s/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/sdmmc/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_sdmmc/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_sdspi/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_sdio/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_dac/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_rmt/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_tsens/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_sdm/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_i2c/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_ledc/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_driver_parlio/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/driver/deprecated"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/driver/i2c/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/driver/touch_sensor/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/driver/twai/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/driver/touch_sensor/esp32s3/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/nvs_flash/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_phy/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_phy/esp32s3/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/wpa_supplicant/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/wpa_supplicant/port/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/wpa_supplicant/esp_supplicant/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_coex/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_wifi/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_wifi/wifi_apps/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_wifi/wifi_apps/nan_app/include"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/esp_wifi/include/local"
+  "C:/Espressif/frameworks/esp-idf-v5.3.1-2/components/console"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/tiori/workspace/Practice3_752964/coap_server/main/coap_server_example_main.c" "esp-idf/main/CMakeFiles/__idf_main.dir/coap_server_example_main.c.obj" "gcc" "esp-idf/main/CMakeFiles/__idf_main.dir/coap_server_example_main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
